45b700b3d62f68bb2c62e6d28ee8e87b5c8959fa
yufyaj <yk1994k@gmail.com>
2024-03-10 02:59:53 +0000

    初期コミット（都道府県→価格検索機能付与）

---
+++ confirm.php
@@ -0,0 +1,43 @@
+<?php
+    $type = $_GET['type'];
+    $state = $_GET['state'];
+    $city = $_GET['city'];
+    $busu = $_GET['busu'];
+    $price = $_GET['price'];
+    $company = $_GET['company'];
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $privacy = $_GET['privacy'];
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <label>type: </label><label><?= $type ?></label>
+    <label>state: </label><label><?= $state ?></label>
+    <label>city: </label><label><?= $city ?></label>
+    <label>busu: </label><label><?= $busu ?></label>
+    <label>company: </label><label><?= $company ?></label>
+    <label>name: </label><label><?= $name ?></label>
+    <label>mail: </label><label><?= $mail ?></label>
+    <label>privacy: </label><label><?= $privacy ?></label>
+    <from>
+        <input type="hidden" name="type" value="<?= $type ?>" />
+        <input type="hidden" name="state" value="<?= $state ?>" />
+        <input type="hidden" name="city" value="<?= $city ?>" />
+        <input type="hidden" name="busu" value="<?= $busu ?>" />
+        <input type="hidden" name="price" value="<?= $price ?>" />
+        <input type="hidden" name="company" value="<?= $company ?>" />
+        <input type="hidden" name="name" value="<?= $name ?>" />
+        <input type="hidden" name="mail" value="<?= $mail ?>" />
+        <div>
+            <a href="/?type=<?= $type ?>&state=<?= $state ?>&city=<?= $city ?>&busu=<?= $busu ?>&price=<?= $price ?>&company=<?= $company ?>&name=<?= $name ?>&mail=<?= $mail ?>&privacy=true">戻る</a>
+        </div>
+    </from>
+</body>
+</html>
