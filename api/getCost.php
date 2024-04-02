d0b51bf5624196cc5de5d910c4820d40d2b8ee16
yufyaj <yk1994k@gmail.com>
2024-04-02 07:51:57 +0000

    DDD対応に変更

--- api/getCost.php
+++ api/getCost.php
@@ -1,7 +1,12 @@
 <?php
 
-require $_SERVER['DOCUMENT_ROOT'].'/lib/php/Logger/logger.php';
-require $_SERVER['DOCUMENT_ROOT'].'/lib/php/DB/db.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/lib/php/Logger/logger.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/lib/php/DB/db.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/model/InputItem/InputItem.class.php';
+$pattern = $_SERVER['DOCUMENT_ROOT'] . '/model/InputItem/*.php';
+foreach (glob($pattern) as $filename) {
+    require_once $filename;
+}
 
 use lib\Logger as logger;
 use lib\db as db;
@@ -9,47 +14,47 @@ use lib\db as db;
 $log = logger\Logger::getInstance();
 $db = db\ConnectDb::getInstance();
 
-if($_SERVER["REQUEST_METHOD"] != "GET"){
+if ($_SERVER["REQUEST_METHOD"] != "GET") {
     // ブラウザからHTMLページを要求された場合
-}else{
+} else {
     // フォームからGETによって要求された場合
 
     $type = $_GET['type'];
     $state = $_GET['state'];
     $city = $_GET['city'];
     $busu = $_GET['busu'];
-    $log->info("type: {$type}, state: {$state}, city: {$city}, busu: {$busu}");
 
-    // 必須入力チェック
-    if (empty($type) || empty($state) || empty($city) || empty($busu)) {
-        $result = array('result' => false,
-                        'is_empty_type' => empty($type),
-                        'is_empty_state' => empty($state),
-                        'is_empty_city' => empty($city),
-                        'is_empty_busu' => empty($busu),
-                        'cost' => 0);
+    $inputItem = new InputItem();
+    $inputItem->setType($type);
+    $inputItem->setState($state);
+    $inputItem->setCity($city);
+    $inputItem->setBusu($busu);
 
-        echo json_encode($result);
+    $responseJson = $inputItem->getResponseJson();
+    if (!json_decode($responseJson)->Result) {
+        echo $responseJson;
         return;
     }
-        
+
     //SQLを作成
-    $sql = "SELECT (cost * $busu) AS cost FROM cost WHERE state = '$state' AND city = '$city' AND type = '$type'";
-    
+    $sql = "SELECT (cost * " . $inputItem->getBusu()->getValue() . ") AS cost FROM cost WHERE state = '" . $inputItem->getState()->getValue() . "'
+                                                                                  AND city = '" . $inputItem->getCity()->getValue() . "' 
+                                                                                  AND type = '" . $inputItem->getType()->getValue() . "'";
+
     //$pdoにあるqueryメソッドを呼び出してSQLを実行
     //出力結果を$rowに代入
     $rows = $db->fetchAll($sql);
-    
-    //出力結果をそれぞれの配列に格納
-    $cost = array_column($rows,'cost')[0];
 
-    $result = array('result' => true,
-                    'is_empty_type' => empty($type),
-                    'is_empty_state' => empty($state),
-                    'is_empty_city' => empty($city),
-                    'is_empty_busu' => empty($busu),
-                    'cost' => $cost);
-    echo json_encode($result);    
+    //出力結果をそれぞれの配列に格納
+    $cost = array_column($rows, 'cost')[0];
+    $inputItem->setCost($cost);
+
+    $responseJson = $inputItem->getResponseJson();
+    if (!json_decode($responseJson)->Result) {
+        echo $responseJson;
+        return;
+    }
+
+    echo $responseJson;
     return;
 }
-?>
